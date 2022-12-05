790b0ec73f94a5180edfbaab35b52c864e3a03f8
Mhmmdrzk <118596896+Mhmmdrzk@users.noreply.github.com>
2022-12-05 10:55:29 +0700

    Add files via upload

---
+++ Informatika - Muhammad Rizki Abdussalam - tugas4 Algoritma.cpp
@@ -0,0 +1,34 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+cout << "## Tugas 4 Program C++ Menentukan Ganjil-Genap ##" << endl;
+cout << "====================================================" << endl;
+
+cout << "NAMA : Muhammad Rizki Abdussalam"<< endl;
+cout << "NIM : 5520122045"<< endl;
+cout << endl;
+cout << "====================================================" << endl;
+int angkabilangan;
+cout<<"Menentukan Genap dan Ganjil"<<endl;
+cout<<"Silahkan Masukan Angka : "<<endl;
+cin>>angkabilangan;
+cout<<endl;
+
+cout << "=========================MAKA!======================" << endl;
+
+if (angkabilangan%2==0){
+cout << "Angka "<<angkabilangan<<" Adalah bilangan genap"<<endl;
+}else {
+cout << "Angka "<<angkabilangan<<" Adalah bilangan ganjil"<<endl;
+}
+
+cout << "====================================================" << endl;
+cout << endl;
+return 0;
+
+}
